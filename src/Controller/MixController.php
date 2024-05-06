3d0e2603c58b2d0514cf138d515a25f2ec3c2cbb
emna aissa <emna.aissa2003@mail.com>
2024-05-06 09:32:05 +0100

    Flash Message & Rich vs Anemic Models

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -45,12 +45,16 @@ class MixController extends AbstractController
     public function vote(VinylMix $mix, Request $request, EntityManagerInterface $entityManager): Response
     {
         $direction = $request->request->get('direction', 'up');
+
         if ($direction === 'up') {
-            $mix->setVotes($mix->getVotes() + 1);
+            $mix->upVote();
         } else {
-            $mix->setVotes($mix->getVotes() - 1);
+            $mix->downVote();
         }
+
         $entityManager->flush();
+        $this->addFlash('success', 'Vote counted!');
+
         return $this->redirectToRoute('app_mix_show', [
             'id' => $mix->getId(),
         ]);
